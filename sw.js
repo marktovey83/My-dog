d785e4e2cc3b8ca554e1716f5e292b1de0745506
marktovey83 <marktovey83@gmail.com>
2026-09-11 05:00:40 +0800

    Make SH a website and installable phone app (PWA)

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "sh-v1";
+const PRECACHE = ["./", "./profile.html", "./index.html", "./shop.html", "./css/styles.css", "./manifest.json", "./icon.svg"];
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(PRECACHE); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).then(function (res) {
+    const copy = res.clone();
+    caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+    return res;
+  }).catch(function () { return caches.match(e.request); }));
+});
